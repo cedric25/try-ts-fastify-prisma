549ea33134ade4f3f0341e6bc4ddc7040ce65d78
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-02-09 09:29:00 +0100

    Change fruit prisma ref

--- src/app/plugins/prisma.ts
+++ src/app/plugins/prisma.ts
@@ -1,12 +1,12 @@
 import { FastifyPluginAsync } from 'fastify'
 import fp from 'fastify-plugin'
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 declare module 'fastify' {
   interface FastifyInstance {
-    fruitDao: Prisma.FruitDelegate<any>
+    fruitDao: PrismaClient['fruit']
   }
 }
 
